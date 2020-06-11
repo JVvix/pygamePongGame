29cde49dfff0eca0617c38a89d04d2c470994699
Faye Ly <fayedotly@gmail.com>
2020-06-11 14:19:14 -0700

    encountered logic error that makes ball continue off screen

--- pygame_pong_tutorial.py
+++ pygame_pong_tutorial.py
@@ -9,7 +9,7 @@ clock = pygame.time.Clock()
 
 # set up the main window
 
-screen_width = 900
+screen_width = 500
 screen_height = 500
 screen = pygame.display.set_mode((screen_width, screen_height))
 pygame.display.set_caption('Pong')
@@ -18,6 +18,14 @@ pygame.display.set_caption('Pong')
 ball = pygame.Rect(screen_width/2 - 15, screen_height/2 - 15, 30, 30)
 player = pygame.Rect(screen_width - 20, screen_height/2 - 70, 10, 140)
 opponent = pygame.Rect(10, screen_height/2 - 70, 10, 140)
+
+ball_speed_x = 7
+ball_speed_y = 7
+
+if ball.top <= 0 or ball.bottom >= screen_height:
+    ball_speed_y *= -1
+if ball.left <= 0 or ball.right >= screen_width:
+    ball_speed_x *= -1
 
 bg_color = pygame.Color('grey12')
 light_grey = (200, 200, 200)
@@ -29,12 +37,15 @@ while True:
             pygame.quit()
             sys.exit()
 
+    ball.x += ball_speed_x
+    ball.y += ball_speed_y
+
     # Visuals
     screen.fill(bg_color)
     pygame.draw.rect(screen, light_grey, player)
     pygame.draw.rect(screen, light_grey, opponent)
     pygame.draw.ellipse(screen, light_grey, ball)
-
+    pygame.draw.aaline(screen, light_grey, (screen_width/2, 0), (screen_width/2, screen_height))
 
     # updating the window
     pygame.display.flip()
