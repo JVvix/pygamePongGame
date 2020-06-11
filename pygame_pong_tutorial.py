f033f1ceacdd0a58c93187cd8377cb17dbe3acf7
Faye Ly <fayedotly@gmail.com>
2020-06-11 16:16:52 -0700

    fixed logic error that contiuned off screen and added bounce between pads

--- pygame_pong_tutorial.py
+++ pygame_pong_tutorial.py
@@ -9,23 +9,18 @@ clock = pygame.time.Clock()
 
 # set up the main window
 
-screen_width = 500
-screen_height = 500
+screen_width = 900
+screen_height = 520
 screen = pygame.display.set_mode((screen_width, screen_height))
 pygame.display.set_caption('Pong')
 
 # game rectangles
 ball = pygame.Rect(screen_width/2 - 15, screen_height/2 - 15, 30, 30)
-player = pygame.Rect(screen_width - 20, screen_height/2 - 70, 10, 140)
-opponent = pygame.Rect(10, screen_height/2 - 70, 10, 140)
+player = pygame.Rect(10, screen_height/2 - 70, 10, 140)
+opponent = pygame.Rect(screen_width - 20, screen_height/2 - 70, 10, 140)
 
 ball_speed_x = 7
 ball_speed_y = 7
-
-if ball.top <= 0 or ball.bottom >= screen_height:
-    ball_speed_y *= -1
-if ball.left <= 0 or ball.right >= screen_width:
-    ball_speed_x *= -1
 
 bg_color = pygame.Color('grey12')
 light_grey = (200, 200, 200)
@@ -39,6 +34,14 @@ while True:
 
     ball.x += ball_speed_x
     ball.y += ball_speed_y
+
+    if ball.top <= 0 or ball.bottom >= screen_height:
+        ball_speed_y *= -1
+    if ball.left <= 0 or ball.right >= screen_width:
+        ball_speed_x *= -1
+
+    if ball.colliderect(player) or ball.colliderect(opponent):
+        ball_speed_x *= -1
 
     # Visuals
     screen.fill(bg_color)
