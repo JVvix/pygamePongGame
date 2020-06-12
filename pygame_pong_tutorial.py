bac5748252437cb653de0cf83709016b3a5ab6a7
Faye Ly <fayedotly@gmail.com>
2020-06-11 17:30:37 -0700

    finished pt 1 video of pong tutorial: added player movement and opponent AI

--- pygame_pong_tutorial.py
+++ pygame_pong_tutorial.py
@@ -2,6 +2,35 @@
 
 import pygame, sys
 
+def ball_animation():
+    global ball_speed_x, ball_speed_y
+    ball.x += ball_speed_x
+    ball.y += ball_speed_y
+
+    if ball.top <= 0 or ball.bottom >= screen_height:
+        ball_speed_y *= -1
+    if ball.left <= 0 or ball.right >= screen_width:
+        ball_speed_x *= -1
+
+    if ball.colliderect(player) or ball.colliderect(opponent):
+        ball_speed_x *= -1
+
+def player_animation():
+    player.y += player_speed
+    if player.top  <= 0:
+        player.top = 0
+    if player.bottom >= screen_height:
+        player.bottom = screen_height
+def opponent_ai():
+    if opponent.top < ball.y:
+        opponent.top += opponent_speed
+    if opponent.bottom > ball.y:
+        opponent.bottom -= opponent_speed
+    if opponent.top  <= 0:
+        opponent.top = 0
+    if opponent.bottom >= screen_height:
+        opponent.bottom = screen_height
+
 # general set-up
 
 pygame.init()
@@ -21,6 +50,8 @@ opponent = pygame.Rect(screen_width - 20, screen_height/2 - 70, 10, 140)
 
 ball_speed_x = 7
 ball_speed_y = 7
+player_speed = 0
+opponent_speed = 7
 
 bg_color = pygame.Color('grey12')
 light_grey = (200, 200, 200)
@@ -32,16 +63,24 @@ while True:
             pygame.quit()
             sys.exit()
 
-    ball.x += ball_speed_x
-    ball.y += ball_speed_y
+        if event.type == pygame.KEYDOWN:
+            if event.key == pygame.K_DOWN:
+                player_speed += 7
+            if event.key == pygame.K_UP:
+                player_speed -= 7
+        if event.type == pygame.KEYUP:
+            if event.key == pygame.K_DOWN:
+                player_speed -= 7
+            if event.key == pygame.K_UP:
+                player_speed += 7
 
-    if ball.top <= 0 or ball.bottom >= screen_height:
-        ball_speed_y *= -1
-    if ball.left <= 0 or ball.right >= screen_width:
-        ball_speed_x *= -1
 
-    if ball.colliderect(player) or ball.colliderect(opponent):
-        ball_speed_x *= -1
+    ball_animation()
+    player_animation()
+    opponent_ai()
+
+    # events
+
 
     # Visuals
     screen.fill(bg_color)
